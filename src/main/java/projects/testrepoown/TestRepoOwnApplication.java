477cded4f905b3978d4d74d8c1bf39102096a31e
Jakub Gembiak <jakub.gembiak@gmail.com>
2020-11-21 11:29:34 +0100

    structure :: initial project structure

---
+++ src/main/java/projects/testrepoown/TestRepoOwnApplication.java
@@ -0,0 +1,13 @@
+package projects.testrepoown;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestRepoOwnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestRepoOwnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/projects/testrepoown/TestRepoOwnApplicationTests.java
@@ -0,0 +1,13 @@
+package projects.testrepoown;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestRepoOwnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
